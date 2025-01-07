798405306765e4f8fdfe735fe2cd3d166d239485
ZeSved <timothy.korell@gmail.com>
2025-01-07 22:56:56 +0100

    successful communication between popup and page

--- d.js
+++ d.js
@@ -1,11 +1,3 @@
-// export const d = {
-// 	test: 'yay',
-// }
-
-// globalThis.da = {
-// 	test: 'yay',
-// }
-
-let t = 'test'
-// const t = 'test'
-// let t = 'test'
+const d = {
+	name: 'test',
+}
--- index.js
+++ index.js
@@ -1,71 +1,35 @@
 // import { d } from './d.js'
+// const d = chrome.extension.getBackgroundPage().d
 
 setInterval(async () => {
-	document.querySelectorAll('ytd-rich-item-renderer, ytd-video-renderer').forEach((video) => {
-		JSON.parse(localStorage.getItem('no-spoilers-config') ?? '[]').forEach((word) => {
-			if (elm.querySelector('yt-formatted-string')?.textContent?.toLowerCase().includes(word)) {
-				elm.style.opacity = '20%'
-			}
-		})
+	const v = await browser.storage.local.get('value')
+	// console.log(v)
+	document.querySelectorAll('ytd-rich-item-renderer, ytd-video-renderer').forEach((elm) => {
+		if (elm.querySelector('yt-formatted-string')?.textContent?.toLowerCase().includes(v.value)) {
+			elm.style.filter = 'blur(5rem)'
+			// elm.style.opacity = '100%'
+		} else {
+			elm.style.opacity = '100%'
+		}
+		// JSON.parse(localStorage.getItem('no-spoilers-config') ?? '[]').forEach((word) => {
+		// })
 	})
-	// globalThis.da = globalThis.da
-
-	// const im = await import('./d.js')
-	// import('/d.json').then((r) => {
-	// 	console.log(r)
-	// })
-	// console.log(globalThis.da.test)
-	// console.log(window.wrappedJSObject.t)
-	// console.log(
-	// 	chrome.storage.sync.get('test', function (data) {
-	// 		console.log(data.test)
-	// 	})
-	// )
-	// chrome.storage.sync.get('test', function (data) {
-	// 	console.log(data.test)
-	// })
-	// localStorage.setItem('tme', JSON.stringify(Date.now()))
-
-	// console.log(parseInt(localStorage.getItem('tme')!) + 1)
 }, 500)
 
-// document.getElementById('bt')?.addEventListener('click', () => {
-//   alert('hello')
-// })
-
-function onLoad(e) {
-	localStorage.setItem('testt', JSON.stringify(e))
-}
-
-window.addEventListener('message', (e) => {
-	if (e.origin === 'http://localhost:5173') {
-		localStorage.setItem('ttttt', JSON.stringify(e))
-		console.log(e)
-	}
-	console.log(e)
-	// await document.requestStorageAccess()
-})
-
-// window.addEventListener('load', () => {
-// 	localStorage.setItem('t', 'e')
-// 	setTimeout(() => {
-// 		const e = window.prompt('words')
-// 		console.log(e)
-// 	}, 5000)
-// 	window.postMessage('test', '*')
-// 	br
-// })
-
-browser.runtime.onMessage.addListener((message) => {
-	if (message.command === 'now') {
-		console.log('yo')
-	} else if (message.command === 'reset') {
-		console.log('test please')
+browser.storage.onChanged.addListener((changes, area) => {
+	if (area === 'local' && 'value' in changes) {
+		document.querySelectorAll('ytd-rich-item-renderer, ytd-video-renderer').forEach(async (elm) => {
+			if (
+				elm
+					.querySelector('yt-formatted-string')
+					?.textContent?.toLowerCase()
+					.includes(changes.value.newValue)
+			) {
+				elm.style.opacity = '20%'
+			}
+			// JSON.parse(localStorage.getItem('no-spoilers-config') ?? '[]').forEach((word) => {
+			// })
+		})
+		console.log(changes.value.newValue)
 	}
 })
-
-// window.addEventListener('click', (e) => {
-// 	// localStorage.setItem('ttttt', e.data)
-// 	// console.log(e.data)
-// 	console.log(e.target)
-// })
